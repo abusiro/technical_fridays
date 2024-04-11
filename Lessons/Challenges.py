c2d9321ef511e51f534254cf19da2d3484d35e03
manuel.pellicer <manuel.pellicer@col.vueling.com>
2024-04-11 14:35:41 +0200

    completado enunciados

--- Lessons/Challenges.py
+++ Lessons/Challenges.py
@@ -1,4 +1,6 @@
 """
 1. funcion que devuelva un booleano indicando si un string es un palindromo
-2. Funcion que devuelva el máximo numero de 0 que hay en un numero binario introducido como un string
+2. Funcion que devuelva el máximo numero de 0s que hay en un numero binario introducido como un string
+3. Función que de tome como input un diccionario y devuelva otro diccionario en el que las claves del input son los
+valores del iccionario output y los valores del input son los claves del diccionario output
 """
